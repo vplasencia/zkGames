7afdb28b203ac7f71895f95808b568ae286ac31a
vplasencia <v.pcalana@gmail.com>
2022-04-12 17:17:09 -0400

    Remove event from the smart contract

--- contracts/contracts/Sudoku.sol
+++ contracts/contracts/Sudoku.sol
@@ -28,8 +28,6 @@ contract Sudoku is ERC721URIStorage {
     string svgSudokuNft =
         '<svg width="350" height="350" viewBox="0 0 350 350" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="a" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#9333ea"/><stop offset="1" stop-color="#4f46e5" stop-opacity=".99"/></linearGradient></defs><rect width="100%" height="100%" fill="url(#a)"/><text x="80%" y="95%" style="fill:#0f172a;font-family:serif;font-size:15px">zkGames</text><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" style="fill:#e2e8f0;font-family:serif;font-size:30px">Sudoku</text></svg>';
 
-    event NewSudokuNFTMinted(address sender, uint256 tokenId);
-
     constructor(address _verifierAddr) ERC721("SudokuNFT", "zkGames") {
         verifierAddr = _verifierAddr;
     }
@@ -115,7 +113,6 @@ contract Sudoku is ERC721URIStorage {
             newItemId,
             msg.sender
         );
-        emit NewSudokuNFTMinted(msg.sender, newItemId);
     }
 
     function verifySudokuAndMintNft(
