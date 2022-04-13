9faa469ffffae04d1e07ba26384cdf0b40769994
vplasencia <v.pcalana@gmail.com>
2022-04-13 13:02:19 -0400

    Add custom 500 page

---
+++ zkgames-ui/pages/500.js
@@ -0,0 +1,9 @@
+export default function Custom500() {
+  return (
+    <div className="flex justify-center items-center">
+      <span className="text-3xl mb-5 font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-indigo-500">
+        500 - Server-side error occurred
+      </span>
+    </div>
+  );
+}
