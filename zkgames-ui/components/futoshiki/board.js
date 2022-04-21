f56322c92f5b5c8388bd7ca00ad02ebb72c1a1c2
vplasencia <v.pcalana@gmail.com>
2022-04-21 02:23:10 -0400

    Change inequality icon from ascii to feather icons

--- zkgames-ui/components/futoshiki/board.js
+++ zkgames-ui/components/futoshiki/board.js
@@ -11,13 +11,39 @@ export default function Board({
     if (inequality === 1) {
       return (
         <div className="flex select-none justify-center items-center text-2xl h-10 w-10 md:h-14 md:w-14">
-          &#x276C;
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-chevron-left w-8 h-8"
+          >
+            <polyline points="15 18 9 12 15 6"></polyline>
+          </svg>
         </div>
       );
     } else if (inequality === 2) {
       return (
         <div className="flex select-none justify-center items-center text-2xl h-10 w-10 md:h-14 md:w-14">
-         &#x276D;
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-chevron-right w-8 h-8"
+          >
+            <polyline points="9 18 15 12 9 6"></polyline>
+          </svg>
         </div>
       );
     } else {
@@ -29,14 +55,40 @@ export default function Board({
   const renderInequalitiesCol = (inequality) => {
     if (inequality === 1) {
       return (
-        <div className="flex rotate-90 select-none justify-center items-center text-2xl h-10 w-10 md:h-14 md:w-14">
-          &#x276C;
+        <div className="flex select-none justify-center items-center text-2xl h-10 w-10 md:h-14 md:w-14">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-chevron-up w-8 h-8"
+          >
+            <polyline points="18 15 12 9 6 15"></polyline>
+          </svg>
         </div>
       );
     } else if (inequality === 2) {
       return (
-        <div className="flex rotate-90 select-none justify-center items-center text-2xl h-10 w-10 md:h-14 md:w-14">
-         &#x276D;
+        <div className="flex select-none justify-center items-center text-2xl h-10 w-10 md:h-14 md:w-14">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-chevron-down w-8 h-8"
+          >
+            <polyline points="6 9 12 15 18 9"></polyline>
+          </svg>
         </div>
       );
     } else {
@@ -63,13 +115,15 @@ export default function Board({
                       futoshikiBoolInitial={futoshikiBoolInitial}
                       selectedPosition={selectedPosition}
                     />
-                    {j !== row.length - 1 && (
-                      renderInequalitiesRow(futoshikiInequalitiesInitial[i*(row.length - 1)+j])
-                    )}
+                    {j !== row.length - 1 &&
+                      renderInequalitiesRow(
+                        futoshikiInequalitiesInitial[i * (row.length - 1) + j]
+                      )}
                   </div>
-                  {i !== row.length - 1 && (
-                   renderInequalitiesCol(futoshikiInequalitiesInitial[12 + (i*row.length+j)])
-                  )}
+                  {i !== row.length - 1 &&
+                    renderInequalitiesCol(
+                      futoshikiInequalitiesInitial[12 + (i * row.length + j)]
+                    )}
                 </div>
               );
             })}
