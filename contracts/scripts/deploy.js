f89d1891e475bb9fb6052d046cd2249ee79ff9b0
vplasencia <v.pcalana@gmail.com>
2022-04-24 03:00:16 -0400

    Uncomment some codes

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,44 +1,44 @@
 const main = async () => {
   // --------- Sudoku ---------------------
 
-  // const contractFactorySudokuVerifier = await hre.ethers.getContractFactory(
-  //   "SudokuVerifier"
-  // );
-  // const contractSudokuVerifier = await contractFactorySudokuVerifier.deploy();
-  // await contractSudokuVerifier.deployed();
-  // console.log(
-  //   "SudokuVerifier Contract deployed to:",
-  //   contractSudokuVerifier.address
-  // );
+  const contractFactorySudokuVerifier = await hre.ethers.getContractFactory(
+    "SudokuVerifier"
+  );
+  const contractSudokuVerifier = await contractFactorySudokuVerifier.deploy();
+  await contractSudokuVerifier.deployed();
+  console.log(
+    "SudokuVerifier Contract deployed to:",
+    contractSudokuVerifier.address
+  );
 
-  // const contractFactorySudoku = await hre.ethers.getContractFactory("Sudoku");
-  // const contractSudoku = await contractFactorySudoku.deploy(
-  //   contractSudokuVerifier.address
-  // );
-  // await contractSudoku.deployed();
-  // console.log("Sudoku Contract deployed to:", contractSudoku.address);
+  const contractFactorySudoku = await hre.ethers.getContractFactory("Sudoku");
+  const contractSudoku = await contractFactorySudoku.deploy(
+    contractSudokuVerifier.address
+  );
+  await contractSudoku.deployed();
+  console.log("Sudoku Contract deployed to:", contractSudoku.address);
 
-  // // --------- Futoshiki ---------------------
+  // --------- Futoshiki ---------------------
 
-  // const contractFactoryFutoshikiVerifier = await hre.ethers.getContractFactory(
-  //   "FutoshikiVerifier"
-  // );
-  // const contractFutoshikiVerifier =
-  //   await contractFactoryFutoshikiVerifier.deploy();
-  // await contractFutoshikiVerifier.deployed();
-  // console.log(
-  //   "FutoshikiVerifier deployed to:",
-  //   contractFutoshikiVerifier.address
-  // );
+  const contractFactoryFutoshikiVerifier = await hre.ethers.getContractFactory(
+    "FutoshikiVerifier"
+  );
+  const contractFutoshikiVerifier =
+    await contractFactoryFutoshikiVerifier.deploy();
+  await contractFutoshikiVerifier.deployed();
+  console.log(
+    "FutoshikiVerifier deployed to:",
+    contractFutoshikiVerifier.address
+  );
 
-  // const contractFactoryFutoshiki = await hre.ethers.getContractFactory(
-  //   "Futoshiki"
-  // );
-  // const contractFutoshiki = await contractFactoryFutoshiki.deploy(
-  //   contractFutoshikiVerifier.address
-  // );
-  // await contractFutoshiki.deployed();
-  // console.log("Futoshiki deployed to:", contractFutoshiki.address);
+  const contractFactoryFutoshiki = await hre.ethers.getContractFactory(
+    "Futoshiki"
+  );
+  const contractFutoshiki = await contractFactoryFutoshiki.deploy(
+    contractFutoshikiVerifier.address
+  );
+  await contractFutoshiki.deployed();
+  console.log("Futoshiki deployed to:", contractFutoshiki.address);
 
   // --------- Skyscrapers ---------------------
 
